926bf642ada50ca6845c1b2d3d27d017ed088508
Eugene <eugene.liu.jing.zh@gmail.com>
2017-04-26 21:58:58 +0800

    添加debug模式判断和异常处理

--- django_debug_middleware.py
+++ django_debug_middleware.py
@@ -1,8 +1,3 @@
-# coding: utf-8
-import json
-from django.db import connections
-from django.conf import settings
-
 class SqlMonitorMiddleware(object):
 
     def process_request(self, request):
@@ -10,12 +5,17 @@ class SqlMonitorMiddleware(object):
 
     def process_response(self, request, response):
         print "in sql monitor middleware process response"
+        if not settings.DEBUG:
+            return response
         database_names = settings.DATABASES.keys()
         queries = {}
         for name in database_names:
             for query in connections[name].queries:
-                queries[name] = query
-        content = json.loads(response.content)
-        content['queries'] = queries
-        response.content = json.dumps(content)
+                queries.setdefault(name, []).append(query)
+        try:
+            content = json.loads(response.content)
+            content['queries'] = queries
+            response.content = json.dumps(content)
+        except:
+            pass
         return response
