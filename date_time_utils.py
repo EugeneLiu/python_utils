2e6faba2934efdaaf7ac8889b3a779b9a280332d
Eugene <eugene@dianmi365.com>
2017-06-22 09:36:27 +0800

    添加计算某个日期几个月之后的日期

--- date_time_utils.py
+++ date_time_utils.py
@@ -14,5 +14,10 @@ def get_month_days(year, month):
     # 获取某年某月有多少天
     return calendar.monthrange(year, month)[1]
 
+def get_after_month_date(date, after_month):
+	# 计算某个日期指定月数的日期
+	after_months_date = date + relativedelta(months=+after_month)
+
+
 if __name__ == '__main__':
     pass
